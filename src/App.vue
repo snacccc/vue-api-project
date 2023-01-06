f3bf28eb765b3c8233846bf41394fb5d47d0c61b
snacccc <80086013+snacccc@users.noreply.github.com>
2023-01-06 13:13:40 -0500

    framework

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+ 
+    <div class="app">
+        <RouterLink class="router home" to="/">Home</RouterLink>
+        <header>|</header>
+        <RouterLink class="router about" to="/about">About</RouterLink>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.app {
+    display: flex;
+    flex-direction: row;
+    background-color: var(--primarydark);
+    font-size: 2rem;
+    height: 5rem;
+    align-items: center;
+    margin-bottom: 2rem;
+    border-bottom: 0.5rem solid;
+    border-color: var(--accent);
+    color: var(--accenttwo);
+  }
+  a {
+    text-decoration: none;
+    color: var(--fontcolormain);
+  }
+  .router {
+    margin: 0 3rem;
+  }
+  .router:hover {
+    text-decoration: underline;
+  }
+  .router:active {
+    color: #2e2e82;
+  }
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    
+    <header class="header">
+      This is the about page
+    </header>
+
+    <p class="description">
+      Why are you still here? This website is terrible. I have no idea what I'm doing. I'm just glad at least this is working.
+    </p>
+
+  </div>
+</template>
+
+<style scoped>
+
+  .about {
+    padding: 1rem;
+    color: var(--fontcolormain);
+  }
+
+  .header {
+    font-size: 1.2rem;
+  }
+
+</style>
