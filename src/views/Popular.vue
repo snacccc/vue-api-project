99d85f6e04a34f5bfaf40c72b5c07cc466f4ffb5
snacccc <80086013+snacccc@users.noreply.github.com>
2023-01-20 13:12:20 -0500

    esry45sjy

--- src/views/Popular.vue
+++ src/views/Popular.vue
@@ -8,19 +8,21 @@
 
 <script>
 
-  export default {
-    name: "Popular",
-    data () {
-      return {
-        info: null
-      }
-    },
-    mounted () {
-      axios
-        .get ('https://imdb-api.com/en/API/MostPopularMovies/k_jtrut4iw.json')
-        .then (response => (this.info = response))
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: null
     }
+  },
+  mounted () {
+    axios 
+      .get ('https://imdb-api.com/en/API/Top250Movies/k_jtrut4iw')
+      .then (response => (this.info = response))
   }
+})
+
+
 
 </script>
 
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,97 +1,11 @@
 <template>
-   <div class="home">
-   
-      <div class="search" id="search">
-         <form class="searchBox" id="searchBox">
-         <input type="text" class="searchInput" placeholder="Search..." name="searchInput" id="searchInput" >
-         </form>
 
-      <header class="movieError" id="movieError"></header>
-
-      <section class="movieInfo" id="movieInfo">
-      <p class="movieName" id="movieName"></p>
-      <img class="moviePoster" id="moviePoster" src="" alt="">
-    </section>
-
-      </div>
-
-   </div>
 </template>
 
 <script>
 
-
-   import Button from "../components/Button.vue";
-
-   export default {
-    name: "Search",
-    compontents: {
-        Button,
-    },
-    data() {
-        return {
-            count: 0,
-            clicked: false,
-        };
-    },
-    methods: {
-      getData(x) {
-      let input = x
-      console.log(input)
-      let key = `https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/${x}`;
-      console.log(key);
-      this.accessAPI(key);
-      },
-      accessAPI (key) {
-         const response = fetch(key)
-         console.log(response);
-         const data = response.json;
-         this.getInfo(data);
-      },
-      getInfo (data) {
-
-         try {
-            if (!data.results[0]) {
-               console.log("No data")
-               return;
-            } else {
-               console.log(data);
-            }
-
-         } catch (error) {
-            console.log(error);  
-            return;
-         }
-      }
-    },
-    async created (input) {
-      await this.accessAPI(`https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/${input}`);
-    },
-}
-   
 </script>
 
 <style scoped>
-   .home {
-      padding: 1rem;
-      margin: 1rem;
-      color: var(--fontcolormain);
-   }
-
-   .movieError {
-      opacity: 0;
-      color: red;
-   }
-
-   .movieName {
-      font-size: 3rem;
-   }
-
-   .moviePoster {
-      height: 40rem;
-      width: 30rem;
-      border: 0rem;
-      opacity: 0;
-   }
 
 </style>
