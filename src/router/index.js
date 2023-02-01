7e1d64d87d91f8aaf6fdc2465bd51d6dac728d23
snacccc <80086013+snacccc@users.noreply.github.com>
2023-02-01 13:13:40 -0500

    onto something

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path:'/yes',
+      name: 'yes',
+      component: () => import('../views/Yes.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+   <div class="home">
+      <header class="header">
+         Test
+      </header>
+   
+      <div class="buttons">
+         <Button @click="getJoke()">
+            Generate Joke
+         </Button>
+      </div>
+
+      <div v-if="joke">
+         <p> {{ joke }} </p> 
+      </div>
+
+   </div>
+</template>
+
+<script>
+
+   import Button from "../components/Button.vue";
+
+   export default {
+    name: "home",
+    compontents: {
+        Button,
+    },
+    data() {
+        return {
+            joke: ''
+        };
+    },
+    methods: {
+      async getJoke () {
+         const info= await this.$http.get('')
+         this joke = info.
+      }
+    }
+}
+   
+</script>
+
+<style scoped>
+   .home {
+      padding: 1rem;
+      margin: 1rem;
+   }
+
+   .header {
+      font-size: 1.2rem;
+   }
+
+   .buttons {
+      display: flex;
+      flex-direction: column;
+      ;
+   }
+
+</style>
